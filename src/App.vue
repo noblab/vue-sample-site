4e8119daeee647628484e8fa3d37ddaebe837047
Noboru Nakahara <nakaharanoboru@gmail.com>
2020-04-13 02:21:40 +0900

    typo

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@
       <v-btn text href="https://alis.to/users/cryptoMedNano">Columns</v-btn>
       <v-btn text href="https://twitter.com/cryptomed_nano">Twitter</v-btn>
       <v-btn text href="https://www.linkedin.com/in/%E6%98%87-%E4%B8%AD%E5%8E%9F-901773197/">LinkedIn</v-btn>
-      <v-btn text href="facebook.com/nakahranoboru">Facebook</v-btn>
+      <v-btn text href="https://www.facebook.com/nakaharanoboru">Facebook</v-btn>
     </v-app-bar>
 
     <v-content>
